d98f94f183ff40666a10f491025165b89ef71e51
puretension <rlrlfhtm5@gmail.com>
2024-01-30 00:50:00 +0900

    FlatList✅

--- src/pages/Orders.tsx
+++ src/pages/Orders.tsx
@@ -1,11 +1,26 @@
-import React from 'react';
-import {Text, View} from 'react-native';
-function Order() {
+import React, {useCallback} from 'react';
+import {FlatList, View} from 'react-native';
+import {useSelector} from 'react-redux';
+import {RootState} from '../store/reducer.ts';
+import {Order} from '../slices/order.ts';
+import EachOrder from '../components/EachOrder.tsx';
+function Orders() {
+  const orders = useSelector((state: RootState) => state.order.orders);
+  const renderItem = useCallback(({item}: {item: Order}) => {
+    return <EachOrder item={item} />;
+  }, []);
+
   return (
     <View>
-      <Text>주문</Text>
+      <FlatList
+        data={orders}
+        keyExtractor={item => item.orderId}
+        renderItem={renderItem}
+      />
     </View>
   );
 }
 
-export default Order;
+export default Orders;
+// redux에 있는건 useSelector로 가져옴
+// 서버통신 데이터, 반복문의 경우에는 ScrollView 대신 FlatList 사용
